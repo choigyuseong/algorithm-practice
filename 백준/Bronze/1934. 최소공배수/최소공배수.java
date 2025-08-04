106005162267ae86c29eadf5944d5bfba601c34b
Gyuseong2 <127475589+choigyuseong@users.noreply.github.com>
2025-08-04 22:04:53 +0900

    [Bronze I] Title: 최소공배수, Time: 180 ms, Memory: 14928 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static final boolean DEBUG = false;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = DEBUG
+                ? new BufferedReader(new FileReader("src/input.txt"))
+                : new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            System.out.println(lcm(Long.parseLong(st.nextToken()), Long.parseLong(st.nextToken())));
+        }
+    }
+
+    public static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static long lcm(long a, long b) {
+        return a / gcd(a, b) * b;
+    }
+}
