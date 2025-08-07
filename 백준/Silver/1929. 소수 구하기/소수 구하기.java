9f5088f5361dcde23bbd1c5c45a6c8e2efe7dd85
Gyuseong2 <127475589+choigyuseong@users.noreply.github.com>
2025-08-07 11:36:23 +0900

    [Silver III] Title: 소수 구하기, Time: 1012 ms, Memory: 29928 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static final boolean DEBUG = false;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = DEBUG
+                ? new BufferedReader(new FileReader("src/input.txt"))
+                : new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        for (int i = M; i <= N; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static boolean isPrime(long n) {
+        if (n < 2) {
+            return false;
+        } else if (n == 2) {
+            return true;
+        } else if (n % 2 == 0) {
+            return false;
+        }
+
+        long sqrt = (long) Math.sqrt(n);
+
+        for (long i = 3; i <= sqrt; i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
