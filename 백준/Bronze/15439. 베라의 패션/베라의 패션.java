0e9fa9ebfc08d222b13d5f5c8c285405d4a1f2d3
Gyuseong2 <127475589+choigyuseong@users.noreply.github.com>
2025-09-02 12:59:24 +0900

    [Bronze IV] Title: 베라의 패션, Time: 100 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+    static final boolean DEBUG = false;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = DEBUG
+                ? new BufferedReader(new FileReader("src/input.txt"))
+                : new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        sb.append(N * (N - 1));
+        System.out.println(sb);
+    }
+}
